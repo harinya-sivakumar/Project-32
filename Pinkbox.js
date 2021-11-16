7e09754027d9bfe8a7dbdd2b4d2ae862a361f732
harinya-sivakumar <73656433+harinya-sivakumar@users.noreply.github.com>
2021-11-16 19:33:20 +0530

    Add files via upload

---
+++ Pinkbox.js
@@ -0,0 +1,43 @@
+class Pinkbox {
+  constructor(x, y, width, height){
+      var options = {
+          'restitution':0.8,
+          'friction':0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+
+      this.visibility = 255;
+    
+  }
+
+  display(){
+    //if(this.body.speed < 5){
+      var pos =this.body.position;
+    var angle = this.body.angle;
+  push();
+  translate(pos.x, pos.y);
+  rotate(angle);
+  rectMode(CENTER);
+  
+  fill(240, 40, 200);
+  rect(0, 0, this.width, this.height);
+  pop();
+  //  }
+    //else{
+      //World.remove(world, this.body);
+      //push();
+      //this.visibility = this.visibility - 1;
+     // tint(255, this.visibility);
+      //console.log(this.visibility);
+   // }
+    
+    }
+
+};
+
+  //fill(240, 40, 200)
